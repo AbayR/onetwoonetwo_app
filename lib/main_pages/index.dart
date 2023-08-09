56b3d8a1afeb9799cb610b33de90b4f2a5dae8b1
Abay <62003749+AbayR@users.noreply.github.com>
2023-08-09 11:16:13 +0600

    Add files via upload

---
+++ lib/main_pages/index.dart
@@ -0,0 +1,60 @@
+// Export pages
+export '/auth_pages/onboarding_page/onboarding_page_widget.dart'
+    show OnboardingPageWidget;
+export '/auth_pages/email_page/email_page_widget.dart' show EmailPageWidget;
+export '/auth_pages/password_page/password_page_widget.dart'
+    show PasswordPageWidget;
+export '/auth_pages/create_account_page/create_account_page_widget.dart'
+    show CreateAccountPageWidget;
+export '/auth_pages/set_pin_page/set_pin_page_widget.dart'
+    show SetPinPageWidget;
+export '/chat/chat_page/chat_page_widget.dart' show ChatPageWidget;
+export '/auth_pages/on_boarding/on_boarding_widget.dart' show OnBoardingWidget;
+export '/admin/create_update/q_apage/q_apage_widget.dart' show QApageWidget;
+export '/admin/create_update/create_bot/create_bot_widget.dart'
+    show CreateBotWidget;
+export '/main_pages/profile_page/profile_page_widget.dart'
+    show ProfilePageWidget;
+export '/main_pages/main_page/main_page_widget.dart' show MainPageWidget;
+export '/library/my_library_page/my_library_page_widget.dart'
+    show MyLibraryPageWidget;
+export '/library/modules_page/modules_page_widget.dart' show ModulesPageWidget;
+export '/library/lessons_page/lessons_page_widget.dart' show LessonsPageWidget;
+export '/library/tactics_page/tactics_page_widget.dart' show TacticsPageWidget;
+export '/admin/create_update/create_course_page/create_course_page_widget.dart'
+    show CreateCoursePageWidget;
+export '/chat/bot_detail_page/bot_detail_page_widget.dart'
+    show BotDetailPageWidget;
+export '/chat/wantu_chat/wantu_chat_widget.dart' show WantuChatWidget;
+export '/chat/wantu_page/wantu_page_widget.dart' show WantuPageWidget;
+export '/library/course_details_page/course_details_page_widget.dart'
+    show CourseDetailsPageWidget;
+export '/library/search_page/search_page_widget.dart' show SearchPageWidget;
+export '/admin/create_update/create_module_page/create_module_page_widget.dart'
+    show CreateModulePageWidget;
+export '/main_pages/list_chats_page/list_chats_page_widget.dart'
+    show ListChatsPageWidget;
+export '/profile/awards_page/awards_page_widget.dart' show AwardsPageWidget;
+export '/profile/profile_information_page/profile_information_page_widget.dart'
+    show ProfileInformationPageWidget;
+export '/profile/change_language_page/change_language_page_widget.dart'
+    show ChangeLanguagePageWidget;
+export '/profile/privacy_page/privacy_page_widget.dart' show PrivacyPageWidget;
+export '/profile/confirm_delete_account/confirm_delete_account_widget.dart'
+    show ConfirmDeleteAccountWidget;
+export '/profile/sms_code_delete_account/sms_code_delete_account_widget.dart'
+    show SmsCodeDeleteAccountWidget;
+export '/profile/notification_settings_page/notification_settings_page_widget.dart'
+    show NotificationSettingsPageWidget;
+export '/profile/contact_support_page/contact_support_page_widget.dart'
+    show ContactSupportPageWidget;
+export '/profile/about_page/about_page_widget.dart' show AboutPageWidget;
+export '/admin/admin_panel/admin_panel_widget.dart' show AdminPanelWidget;
+export '/admin/list_of_players/list_of_players_widget.dart'
+    show ListOfPlayersWidget;
+export '/admin/list_of_courses/list_of_courses_widget.dart'
+    show ListOfCoursesWidget;
+export '/admin/list_of_modules/list_of_modules_widget.dart'
+    show ListOfModulesWidget;
+export '/admin/list_of_chats/list_of_chats_widget.dart' show ListOfChatsWidget;
+export '/auth_pages/email_reg/email_reg_widget.dart' show EmailRegWidget;
